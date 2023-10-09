28777e287462b0ea663c796dcad0775c9e3ce446
Artem Safin <artemsafin.webdev@gmail.com>
2023-10-09 12:40:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = Array.from(
+    document.querySelectorAll(".faq__accordion, .delivery__accordion")
+  );
+
+  console.log("Accordions", accordions);
+
+  accordions.forEach((item) => {
+    const btn = item.querySelector("button");
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      accordions.forEach((otherItem) => {
+        if (otherItem !== item) otherItem.classList.remove("active");
+      });
+      item.classList.toggle("active");
+    });
+  });
+});
